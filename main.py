26865245840c49226603102f21141353e971bc38
nagapriyanc <nagapriyan.c1@gmail.com>
2022-05-16 11:51:56 +0530

    'ok'

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path='')
 
+#num of execution
+count = 5
 
-for i in range(3):
+for i in range(count):
 	print(i)
 	driver.get("url of your site")
+	#take some break
 	time.sleep(5)
 
+#close browser
 driver.quit()	
